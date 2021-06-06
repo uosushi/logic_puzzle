118d9fdb3dea6931b548b16200d1931b84b1e1f5
uosushi <uosushi@users.noreply.github.com>
2021-06-06 23:20:09 +0900

    fix moveRocket(): Skip extra processing when an out-of-range access (i.e. game over) is made or the game is cleared.
    fix moveRocket(): When I put some numbers in the re-paint option, I noticed that there was a mistake in the conditional statement 'tiles[rocketY][rocketX]'.
    
    add skip(): Replaced the uncool code that used only setTimeout with asynchronous processing.
    
    add moveRocket(): JSDoc

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ var tileWidth = 30;
 var tileHeight = 30;
 var tilePadding = 5;
 var tiles;
-var star = 0;
+var star;
+var flag;
+var speed;
+var f1 = [[1, 0],[]];
 
 var tilechip = new Image();
 tilechip.src = './tilechip.png';
@@ -16,24 +19,34 @@ var rocketX, rocketY, rocketRotate;
 var rocket = new Image();
 rocket.src = './rocket.png';
 
-var speed = 700;
+var speed = 500;
 
 // max: 10(width)x20(height)
 
-var level1_map = [
+const level1_map = [
 	[5, 2, 2, 2, 5]
 ];
-
+const level2_map = [
+	[-1, -1, -1,  2,  5],
+	[-1, -1,  2,  2, -1],
+	[-1,  2,  2, -1, -1],
+	[ 2,  2, -1, -1, -1],
+	[ 2, -1, -1, -1, -1]
+];
 
 function drawTiles(set_tiles)
 {
-	tiles = set_tiles;
+	tiles = JSON.parse(JSON.stringify(set_tiles));
 	context.clearRect(0, 0, 800, 400);
 	star = 0;
 	for (y = 0; y < tiles.length; y++)
 	{
 		for (x = 0; x < tiles[y].length; x++)
 		{
+			if (tiles[y][x] == -1)
+			{
+				continue ;
+			}
 			var tileY = y * (tileHeight + tilePadding) + tileOffsetTop;
 			var tileX = x * (tileWidth  + tilePadding) + tileOffsetLeft;
 			context.drawImage(tilechip, tiles[y][x] * tileWidth, 0, tileWidth, tileHeight, tileX, tileY, tileWidth, tileHeight);
@@ -60,10 +73,19 @@ function drawRocket(y, x, rotate)
 	context.restore();
 }
 
-// 操作を反映させる
-// commandが1のときは直進
-function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1)
+/**
+ * Rocketの操作を反映させる関数  
+ * - 引数`command`は直進するとき1の値をもつ  
+ * - 引数`rotate`は方向転換の増減分（+1で90度右, -1で90度左）  
+ * - 引数`opt_conditions`は初期値として-1の値をもち、移動元のタイルと比較して操作を実行するか決める  
+ * - 引数`opt_paint`は初期値として-1の値をもち、値を代入されたときは移動先のタイルを指定された色に塗りかえる。  
+ */
+async function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1, opt_fn = -1)
 {
+	if (flag)
+	{
+		return ;
+	}
 	// x軸とy軸の増減をrotate考慮で算出
 	var y, x;
 	if (rocketRotate % 4 == 1)
@@ -87,7 +109,6 @@ function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1)
 		x = 0;
 	}
 
-
 	// 移動元のRocketとタイルは操作に寄らず一度消去
 	context.clearRect((rocketX - 1) * (tileWidth + tilePadding) + tileOffsetLeft, (rocketY - 1) * (tileHeight + tilePadding) + tileOffsetTop, tileWidth, tileHeight);
 
@@ -97,7 +118,7 @@ function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1)
 	context.drawImage(tilechip, tiles[rocketY - 1][rocketX - 1] * tileWidth, 0, tileWidth, tileHeight, tileX, tileY, tileWidth, tileHeight);
 
 	// 移動元のタイルが操作条件に合っていれば、移動後のRocketの座標を記録
-	if (opt_conditions == -1 || opt_conditions == tiles[rocketY][rocketX] || opt_conditions+3 == tiles[rocketY][rocketX])
+	if (opt_conditions == -1 || opt_conditions == tiles[rocketY - 1][rocketX - 1] || opt_conditions+3 == tiles[rocketY - 1][rocketX - 1])
 	{
 		// 移動後の座標を記録
 		rocketY += y;
@@ -111,7 +132,7 @@ function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1)
 		tileX = (rocketX - 1) * (tileWidth  + tilePadding) + tileOffsetLeft;
 
 		// 移動先のタイルに星があればタイルを星なしのものへ変更
-		if (tiles[rocketY - 1][rocketX - 1]>=3)
+		if (tiles[rocketY - 1][rocketX - 1] >= 3)
 		{
 			tiles[rocketY - 1][rocketX - 1] -= 3;
 			star -= 1;
@@ -132,37 +153,71 @@ function moveRocket(command, rotate, opt_conditions = -1, opt_paint = -1)
 	context.restore();
 	rocketRotate += rotate;
 	console.log(star);
-	setTimeout( function() {
-		if (star==0)
-		{
-			alert("Game clear!!");
-			return ;
-		}
-		if (rocketY > tiles.length || rocketX < 0 || rocketX > tiles[0].length || rocketX < 0)
-		{
-			alert("RANGE OVER!");
-			drawTiles(level1_map);
-			drawRocket(1, 3, 1);
-			return ;
-		}
-	}, 500);
+
+	await sleep(500);
+	if (rocketY > tiles.length || rocketX < 0 || rocketX > tiles[0].length || rocketX < 0)
+	{
+		alert("RANGE OVER!");
+		drawTiles(level1_map);
+		drawRocket(1, 3, 1);
+		flag = 1;
+	}
+	else if (star == 0)
+	{
+		alert("Game clear!!");
+		flag = 1;
+	}
+	return ;
 }
 
-function testFn() {
+function sleep(ms) {
+	if (flag)
+	{
+		return ;
+	}
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function testFn() {
+	flag = 0;
 	// 前に1マス進む
+	await sleep(speed);
 	moveRocket(1, 0);
+
+	// 青を緑に塗りつぶし
+	await sleep(speed);
+	moveRocket(0, 0, 2, 1);
+
 	// 前に1マス進む
-	setTimeout( function() {moveRocket(1, 0);}, speed *1);
+	await sleep(speed);
+	moveRocket(1, 0);
+
 	// 右に方向転換
-	setTimeout( function() {moveRocket(0, 1);}, speed *2);
+	await sleep(speed);
+	moveRocket(0, 1);
+
 	// 右に方向転換
-	setTimeout( function() {moveRocket(0, 1);}, speed *3);
+	await sleep(speed);
+	moveRocket(0, 1);
+
 	// 前に1マス進む
-	setTimeout( function() {moveRocket(1, 0);}, speed *4);
+	await sleep(speed);
+	moveRocket(1, 0);
+
+	// 緑を青に塗りつぶし
+	await sleep(speed);
+	moveRocket(0, 0, 1, 2);
+
 	// 前に1マス進む
-	setTimeout( function() {moveRocket(1, 0);}, speed *5);
+	await sleep(speed);
+	moveRocket(1, 0);
+
 	// 前に1マス進む
-	setTimeout( function() {moveRocket(1, 0);}, speed *6);
+	await sleep(speed);
+	moveRocket(1, 0);
+	
+
 	// 前に1マス進む
-	setTimeout( function() {moveRocket(1, 0);}, speed *7);
+	await sleep(speed);
+	moveRocket(1, 0);
 }
